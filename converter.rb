6e01044596d84150e8a98e14bc7bb6fabd9d2e8f
Icicle technologies <srinidhi.prabandham@icicletech.com>
2017-02-24 15:28:41 +0530

    [Initial commit]  - Project files setup.

---
+++ converter.rb
@@ -0,0 +1,5 @@
+#!/usr/bin/env ruby
+
+#TODO main class that will do the conversion of numbers to meaning full words.
+class Converter
+end
---
+++ spec/converter_spec.rb
@@ -0,0 +1,8 @@
+require_relative '../converter.rb'
+
+#This will do the testing of our convert class.
+#
+#
+describe Converter do
+  #TODO
+end
