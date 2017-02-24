aacd123b692694eaab35b96a9542e49dc114d5a8
Icicle technologies <srinidhi.prabandham@icicletech.com>
2017-02-24 16:52:01 +0530

    genration combination

--- converter.rb
+++ converter.rb
@@ -1,5 +1,46 @@
 #!/usr/bin/env ruby
+#
+require 'set'
 
 #TODO main class that will do the conversion of numbers to meaning full words.
 class Converter
+
+  attr_accessor :letters, :word_list
+
+  def initialize
+    letters; word_list
+  end
+
+  # Hash which we will use to generate the combinations for a given number
+  def letters
+    @letters ||= {
+      "2" => ['a','b','c'],
+      "3" => ['d','e','f'],
+      "4" => ['g','h','i'],
+      "5" => ['j','k','l'],
+      "6" => ['m','n','o'],
+      "7" => ['p','q','r','s'],
+      "8" => ['t','u','v'],
+      "9" => ['w','x','y','z']
+    }
+  end
+
+  # Load the list of words in the dictionary. 
+  def word_list
+    @word_list ||= Set.new File.read("dictionary.txt").split("\n").map(&:downcase)
+  end
+
+  # This take a number and generates all possible combination of words using
+  # Cartesian product
+  def form_word(number = "567")
+    array_of_letters = number.chars.map do |n|
+      @letters[n]
+    end
+
+    combination = array_of_letters[1..-1].inject(array_of_letters.first) do |result, value|
+      result = result.product(value).map(&:flatten)
+    end
+  end
 end
+
+Converter.new.form_word
--- spec/converter_spec.rb
+++ spec/converter_spec.rb
@@ -9,4 +9,20 @@ describe Converter do
       expect(Converter.new.word_list).not_to be []
     end
   end
+
+  describe "#numbers to chars - hash_map" do
+    it "Should contain a list of numbers between 2 and 9" do
+      expect(Converter.new.letters.keys).to eq ["2", "3", "4", "5", "6", "7", "8", "9"]
+    end
+
+    it "Should contain the correct sequence for a given number" do
+      expect(Converter.new.letters["2"]).to eq ["a", "b", "c"]
+    end
+  end
+
+  describe "#number to word" do
+    it "Should convert a given number to a sequence of characters" do
+      expect(Converter.new.form_word("567")).to eq [["j", "m", "p"], ["j", "m", "q"], ["j", "m", "r"], ["j", "m", "s"], ["j", "n", "p"], ["j", "n", "q"], ["j", "n", "r"], ["j", "n", "s"], ["j", "o", "p"], ["j", "o", "q"], ["j", "o", "r"], ["j", "o", "s"], ["k", "m", "p"], ["k", "m", "q"], ["k", "m", "r"], ["k", "m", "s"], ["k", "n", "p"], ["k", "n", "q"], ["k", "n", "r"], ["k", "n", "s"], ["k", "o", "p"], ["k", "o", "q"], ["k", "o", "r"], ["k", "o", "s"], ["l", "m", "p"], ["l", "m", "q"], ["l", "m", "r"], ["l", "m", "s"], ["l", "n", "p"], ["l", "n", "q"], ["l", "n", "r"], ["l", "n", "s"], ["l", "o", "p"], ["l", "o", "q"], ["l", "o", "r"], ["l", "o", "s"]]
+    end
+  end
 end
